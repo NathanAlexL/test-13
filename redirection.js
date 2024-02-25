7f9b1fc2a3d18ccd8eee39944449f2d47d49b9a1
NathanAlexL <161165627+NathanAlexL@users.noreply.github.com>
2024-02-25 20:35:30 +0400

    Create redirection.js

---
+++ redirection.js
@@ -0,0 +1,5 @@
+
+// Fonction pour rediriger l'utilisateur vers une page blanche
+function redirectToBlankPage() {
+    window.location.href = "page_blanche.html"; // Remplacez "page_blanche.html" par le nom de votre page blanche
+}
